f9b8cbae30b2bab1460c10a3dcf6e66f885f25e3
KarolChang <super000999888@gmail.com>
2021-11-07 15:27:51 +0800

    fix: expenseTracker

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ bot.on('message', async function (event) {
     console.log('電影', replyMsg)
   }
   if (text.slice(0, 4) === 'JM記帳') {
-    replyMsg = await writeRecord(text)
+    replyMsg = userId === jianmiau ? await writeRecord(text, '建喵') : await writeRecord(text, '豬涵')
     console.log('記帳', replyMsg)
   }
   event.reply(replyMsg).then(data => {
--- functions/expenseTracker.js
+++ functions/expenseTracker.js
@@ -1,25 +1,25 @@
 const axios = require('axios')
 
-const baseUrl = 'https://script.google.com/macros/s/AKfycbyJJv51BtiW6FFvxIGvdzSxSxN7xjsNshOX5jpc1BbHSfFJ1YGsk4GH6-NmXITmG3Ly/exec'
+const baseUrl = 'https://script.google.com/macros/s/AKfycbz8o9qxdFYhDld_RM3oDcEONLFqKlXDg2kibompKpHh-7JJBSQ1rtZcsqqL2ap0rbkf/exec'
 // process.env.GOOGLE_SHEET_URL
 
-function writeRecord(text) {
+function writeRecord(text, person) {
   return new Promise((resolve, reject) => {
     const dataArr = text.split(' ').slice(1)
     if(dataArr.length !== 7) {
       resolve('輸入格式不正確！')
     }
-    const params = `?year=${dataArr[0]}&month=${dataArr[1]}&date=${dataArr[2]}&day=${encodeURIComponent(dataArr[3])}&item=${encodeURIComponent(dataArr[4])}&merchant=${encodeURIComponent(dataArr[5])}&amount=${dataArr[6]}`
+    const params = `?year=${dataArr[0]}&month=${dataArr[1]}&date=${dataArr[2]}&day=${encodeURIComponent(dataArr[3])}&item=${encodeURIComponent(dataArr[4])}&merchant=${encodeURIComponent(dataArr[5])}&amount=${dataArr[6]}&record_person=${encodeURIComponent(person)}&record_time=${new Date().toLocaleString()}`
     console.log('請求網址:', baseUrl + params)
-    // resolve(`請點擊以下連結去寫入資料:\n${baseUrl + params}\n到以下google sheet查看資料總覽跟偷吃豬涵豆腐的建喵算帳:\nhttps://docs.google.com/spreadsheets/d/1vaEXzsvnZotcS88xntc5_DTvF7w1NNJ8bu4dej_4lio/edit#gid=0`)
+    resolve(`請點擊以下連結以寫入資料:\n${baseUrl + params}\n跟偷吃豬涵豆腐的建喵算帳:\nhttps://docs.google.com/spreadsheets/d/1vaEXzsvnZotcS88xntc5_DTvF7w1NNJ8bu4dej_4lio/edit#gid=0`)
 
-    axios.get(baseUrl + params)
-      .then((response) => {
-        resolve('資料已寫入~\n跟偷吃豬涵豆腐的建喵算帳:\nhttps://docs.google.com/spreadsheets/d/1vaEXzsvnZotcS88xntc5_DTvF7w1NNJ8bu4dej_4lio/edit#gid=0')
-      })
-      .catch((err) => {
-        reject('error: ', err)
-      })
+    // axios.get(baseUrl + params)
+    //   .then((response) => {
+    //     resolve('資料已寫入~\n跟偷吃豬涵豆腐的建喵算帳:\nhttps://docs.google.com/spreadsheets/d/1vaEXzsvnZotcS88xntc5_DTvF7w1NNJ8bu4dej_4lio/edit#gid=0')
+    //   })
+    //   .catch((err) => {
+    //     reject('error: ', err)
+    //   })
   })
 }
 
