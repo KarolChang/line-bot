b0fae8af9f4c721f6326b61b2bcec6f9c27f142b
KarolChang <super000999888@gmail.com>
2021-11-07 16:50:37 +0800

    fix: expenseTracker

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ bot.on('message', async function (event) {
   }
   if(text.slice(0, 4) === '記帳加總') {
     console.log('記帳加總text', text)
-    replyMsg = await addAmount()
+    replyMsg = await addAmount(text)
     console.log('記帳加總replyMsg', replyMsg)
   }
   event.reply(replyMsg).then(data => {
