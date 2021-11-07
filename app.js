ef52f6c5d68336aef77869d4e0619e9a28c642c3
KarolChang <super000999888@gmail.com>
2021-11-08 00:18:59 +0800

    fix: expenseTracker

--- app.js
+++ app.js
@@ -32,17 +32,16 @@ bot.on('message', async function (event) {
   }
   if (text.includes('電影')) {
     replyMsg = await movieMsg()
-    bot.push(karol, 'hello')
     console.log('電影', replyMsg)
   }
   if (text.slice(0, 4) === 'JM記帳') {
     console.log('JM記帳', text)
     if(userId === jianmiau) {
       replyMsg = await writeRecord(text, '建喵')
-      // bot.push(karol, `建喵已發佈: ${text}`)
+      bot.push(karol, `建喵已發佈: ${text}`)
     } else {
       replyMsg = await writeRecord(text, '豬涵')
-      // bot.push(jianmiau, `豬涵已發佈: ${text}`)
+      bot.push(jianmiau, `豬涵已發佈: ${text}`)
     }
   }
   if(text.slice(0, 4) === '記帳加總') {
