e0564fc0457b7e692787910d93d3ea5ea6f63775
KarolChang <super000999888@gmail.com>
2021-12-17 09:48:01 +0800

    fix

--- functions/jmExpense.js
+++ functions/jmExpense.js
@@ -2,19 +2,22 @@ import API from '../apis/expense'
 
 async function createRecord(dataArr, person) {
   // ['2021/12/16', '晚餐', '水餃', '八方', '金額', '豬涵']
-  try {
-    const input = {
-      date: dataArr[0],
-      item: dataArr[1],
-      merchant: dataArr[2],
-      amount: dataArr[3],
-      recorder: person
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const input = {
+        date: dataArr[0],
+        item: dataArr[1],
+        merchant: dataArr[2],
+        amount: dataArr[3],
+        recorder: person
+      }
+      await API.create(input)
+      resolve('資料已寫入~\nhttps://karolchang.github.io/jm-expense-vue-ts/record')
+    } catch (err) {
+      reject(`[ERROR]${err}`)
     }
-    await API.create(input)
-    return 'Hello'
-  } catch (error) {
-    console.error('error', error)
-  }
+  })
 }
 
 module.exports = { createRecord }
